f5ae7c06829a28bd1cc6faf4b3064241286dd3c4
Seokyong Jung <syjung@rtcc.hanyang.ac.kr>
2021-06-01 04:42:53 +0900

    removes not available regions.

--- env.sh
+++ env.sh
@@ -26,27 +26,25 @@ else
 fi
 
 # Check available regions:
-#   aws ec2 describe-regions
+#   aws ec2 describe-regions | jq '.["Regions"][]["RegionName"]'
 export AWS_AVAIL_REGIONS=(
-    #'us-east-2' # US East (Ohio)
-    #'us-east-1' # US East (N. Virginia)
-    #'us-west-1' # US West (N. California)
-    #'us-west-2' # US West (Oregon)
-    #'ap-east-1' # Asia Pacific (Hong Kong)
-    #'ap-south-1' # Asia Pacific (Mumbai)
-    #'ap-northeast-3' # Asia Pacific (Osaka-Local)
+    'eu-north-1' # Europe (Stockholm)
+    'ap-south-1' # Asia Pacific (Mumbai)
+    'eu-west-3' # Europe (Paris)
+    'eu-west-2' # Europe (London)
+    'eu-west-1' # Europe (Ireland)
+    'ap-northeast-3' # Asia Pacific (Osaka-Local)
     'ap-northeast-2' # Asia Pacific (Seoul)
-    #'ap-southeast-1' # Asia Pacific (Singapore)
-    #'ap-southeast-2' # Asia Pacific (Sydney)
     'ap-northeast-1' # Asia Pacific (Tokyo)
-    #'ca-central-1' # Canada (Central)
-    #'eu-central-1' # Europe (Frankfurt)
-    #'eu-west-1' # Europe (Ireland)
-    #'eu-west-2' # Europe (London)
-    #'eu-west-3' # Europe (Paris)
-    #'eu-north-1' # Europe (Stockholm)
-    #'me-south-1' # Middle East (Bahrain)
-    #'sa-east-1' # South America (São Paulo)
+    'sa-east-1' # South America (São Paulo)
+    'ca-central-1' # Canada (Central)
+    'ap-southeast-1' # Asia Pacific (Singapore)
+    'ap-southeast-2' # Asia Pacific (Sydney)
+    'eu-central-1' # Europe (Frankfurt)
+    'us-east-1' # US East (N. Virginia)
+    'us-east-2' # US East (Ohio)
+    'us-west-1' # US West (N. California)
+    'us-west-2' # US West (Oregon)
 )
 
 group_exist() {
